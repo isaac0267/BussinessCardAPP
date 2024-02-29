61af38e8bdb404e909c11b54c889ff961dbf531a
Isaac Levy <isaa0267@stud.kea.dk>
2024-02-29 10:34:54 +0100

    I am back

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
+  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Isaac CEO of Boxing</Text>
+      <Image source={require('./assets/card.jpg')}
+      style={styles.image}
+      />
+      <Text style={styles.font}>Company Logo of Isaac</Text>
     </View>
+  
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+image:{
+ width:600,
+ height:400 ,
+ resizeMode:'center',
+ marginVertical:'10',
+ borderRadius: 10 // Add border-radius for a rounded effect
+
+},
+font:{
+  fontSize:20,
+  color:'white',
+  marginVertical:10,
+}
 });
+
